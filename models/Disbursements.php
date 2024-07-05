c3cb9a38e6514bdaa63b46a48e36ae44498dd1b1
walumbe <nathanwalumbe@gmail.com>
2024-07-05 09:59:52 +0300

    remove vardumps

--- models/Disbursements.php
+++ models/Disbursements.php
@@ -359,7 +359,7 @@ class Disbursements extends \yii\db\ActiveRecord
                 $req=json_encode($req);
                 $url=TIGO_PAY_URL;
                 $headers=['Content-Type: application/json','Authorization:'.DEPOSIT_AUTHORIZATION];
-                var_dump(Myhelper::curlPost($req,$headers,$url));
+                Myhelper::curlPost($req,$headers,$url);
             }
 
         }
