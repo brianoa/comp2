19d887181c0928f4f6fc8534910a530fec8fd3d4
EstWandia <esther.ngetha@codeshop.co.ke>
2024-08-22 12:01:04 +0300

    add transactionlog view

---
+++ models/TransactionLog.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "transaction_log".
+ *
+ * @property string $id
+ * @property string $json_data
+ * @property string $date
+ * @property string|null $api_type
+ * @property int $state
+ * @property string|null $transID
+ */
+class TransactionLog extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'transaction_log';
+    }
+    public static function getDb() {
+        return Yii::$app->mpesa_db;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'json_data', 'date'], 'required'],
+            [['json_data'], 'string'],
+            [['date'], 'safe'],
+            [['state'], 'integer'],
+            [['id'], 'string', 'max' => 36],
+            [['api_type'], 'string', 'max' => 20],
+            [['transID'], 'string', 'max' => 50]
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'json_data' => 'Json Data',
+            'date' => 'Date',
+            'api_type' => 'Api Type',
+            'state' => 'State',
+            'transID' => 'Trans ID',
+        ];
+    }
+}
---
+++ views/transactionlog/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\TransactionLog */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="transaction-log-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'json_data')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'date')->textInput() ?>
+
+    <?= $form->field($model, 'api_type')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'state')->textInput() ?>
+
+    <?= $form->field($model, 'transID')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
